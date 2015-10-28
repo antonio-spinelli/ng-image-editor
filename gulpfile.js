db511c298b4d7e3f764048d42e9371f0205e1494
Antonio Spinelli <lordfong86-git@yahoo.it>
2015-10-28 21:31:58 +0100

    Added jshint and gulp

---
+++ gulpfile.js
@@ -0,0 +1,57 @@
+var gulp = require('gulp'),
+	path = require('path'),
+	jshintReporter = require('jshint-stylish'),
+	plugins = require('gulp-load-plugins')({
+		config: path.join(__dirname, 'package.json')
+	});
+
+var path = {
+	src: {
+		files: 'src/**/*.js'
+	}
+};
+
+var pkgName = 'ng-image-editor';
+
+gulp.task('jshint', function(done) {
+	gulp.src(path.src.files)
+	.pipe(plugins.jshint('.jshintrc'))
+	.pipe(plugins.jshint.reporter(jshintReporter));
+	done();
+});
+
+gulp.task('build', function() {
+	var pkg = require('./bower.json');
+
+	var header = ['/**',
+		' * <%= pkg.name %>',
+		' * <%= pkg.description %>',
+		' * @version v<%= pkg.version %>',
+		' * @author <%= pkg.authors[0]%>',
+		' * @link <%= pkg.homepage %>',
+		' * @license <%= pkg.license %>',
+		' */',
+		'(function (angular, $){',
+		'',
+		''].join('\n');
+
+	var footer = [
+		'',
+		'})(angular, jQuery);',
+		''].join('\n');
+
+	gulp.src([
+		'src/**/*.js'
+	])
+	.pipe(plugins.concat(pkgName + '.js'))
+	.pipe(plugins.header(header, {pkg: pkg}))
+	.pipe(plugins.footer(footer))
+	.pipe(gulp.dest('./dist/'))
+	.pipe(plugins.uglify())
+	.pipe(plugins.concat(pkgName + '.min.js'))
+	.pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('default', ['jshint', 'build'], function() {
+	gulp.watch(path.src.files, ['jshint', 'build']);
+});
